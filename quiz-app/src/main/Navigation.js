903915c494c6875bf24b9bfcf4d4ec48ebf1beb0
dominik395 <chojnowski.dominik@wp.pl>
2024-03-02 20:13:46 +0100

    usuniecie nie potrzebnych zeczy(blog(

--- quiz-app/src/main/Navigation.js
+++ quiz-app/src/main/Navigation.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./containers/Home";
-import Blog from "./components/Blog";
 import Template from "./Template";
 import Login from './components/Login'
 import SingUp from "./containers/SingUp";
@@ -14,7 +13,6 @@ const Navigation = () => {
                     <Template>
                         <Routes>
                             <Route path="/" element={<Home />} />
-                            <Route path="/blog" element={<Blog />} />
                             <Route path="/login" element={<Login />} />
                             <Route path="/singUp" element={<SingUp />} />
                             <Route path="/logout" element={<Logout />} />
